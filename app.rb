9454cc63d0f578ed9c6a8c99202331b982cb8a79
Nicholas Smith <nico.ehs05@gmail.com>
2018-10-08 18:35:26 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -22,7 +22,7 @@ class App < Sinatra::Base
   
   get "/say/:word1/:word2/:word3/:word4/:word5" do
     @name = params[:word1]
-    [params[:word1],params[:word1],params[:word1],params[:word1],params[:word1]].join(" ")+"."
+    [params[:word1],params[:word2],params[:word3],params[:word4],params[:word5]].join(" ")+"."
   end
   
   get "/:operation/:number1/:number2" do
